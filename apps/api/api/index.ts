5efa88bc1ab55092b4758b9af761732a932c3c29
Pawel Sowinski <pawel.sowinski.93@gmail.com>
2025-06-19 21:25:17 +0200

    poprawa builda

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -11,3 +11,9 @@ app.get("/", (c) => c.json({ message: "Hello World" }));
 const handler = handle(app);
 
 export const GET = handler;
+export const POST = handler;
+export const PUT = handler;
+export const DELETE = handler;
+export const PATCH = handler;
+export const HEAD = handler;
+export const OPTIONS = handler;
